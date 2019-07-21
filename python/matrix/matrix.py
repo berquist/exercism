895fe6b01d3fedd080e8e49a526ba1aec316718e
Eric Berquist <eric.berquist@gmail.com>
2019-07-21 13:59:06 -0400

    [Python] matrix that uses more memory but indexes in constant time

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,11 +1,17 @@
 class Matrix(object):
     def __init__(self, matrix_string):
-        self._mat = [
-            [int(x) for x in row.split()] for row in matrix_string.splitlines()
-        ]
+        self._rows = []
+        self._cols = []
+        for row in matrix_string.splitlines():
+            row = [int(x) for x in row.split()]
+            self._rows.append(row)
+            while len(self._cols) != len(row):
+                self._cols.append([])
+            for i, e in enumerate(row):
+                self._cols[i].append(e)
 
     def row(self, index):
-        return self._mat[index - 1]
+        return self._rows[index - 1]
 
     def column(self, index):
-        return [row[index - 1] for row in self._mat]
+        return self._cols[index - 1]

bb92488f22c6eb8d82bff0c8cc21eef4e82147c8
Eric Berquist <eric.berquist@gmail.com>
2019-07-21 14:10:40 -0400

    [Python] matrix with slightly better variable names

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -4,12 +4,12 @@ class Matrix(object):
         self._cols = []
         for row in matrix_string.splitlines():
             self._rows.append([])
-            for icol, x in enumerate(row.split()):
-                intx = int(x)
-                self._rows[-1].append(intx)
+            for icol, telement in enumerate(row.split()):
+                element = int(telement)
+                self._rows[-1].append(element)
                 while len(self._cols) <= icol:
                     self._cols.append([])
-                self._cols[icol].append(intx)
+                self._cols[icol].append(element)
 
     def row(self, index):
         return self._rows[index - 1]

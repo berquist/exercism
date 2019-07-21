83bb8ab2b37aeb6e6920fa6914229e9da80d892d
Eric Berquist <eric.berquist@gmail.com>
2019-07-21 15:04:14 -0400

    [Python][twelve-days] double list comprehension

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -32,10 +32,10 @@ NUM_TO_DAYS_LINE = (
 
 
 def recite(start_verse, end_verse):
-    verses = []
-    for verse_num in range(start_verse, end_verse + 1):
-        verse_parts = [FIRST_LINE.format(NUM_TO_ROMAN[verse_num])] + [
-            NUM_TO_DAYS_LINE[verse_part] for verse_part in range(verse_num, 0, -1)
-        ]
-        verses.append("".join(verse_parts))
-    return verses
+    return [
+        "".join(
+            [FIRST_LINE.format(NUM_TO_ROMAN[verse_num])]
+            + [NUM_TO_DAYS_LINE[verse_part] for verse_part in range(verse_num, 0, -1)]
+        )
+        for verse_num in range(start_verse, end_verse + 1)
+    ]

5aadd7d4a8be5bcc0bf5b6d73acb0a9b94f27db7
Eric Berquist <eric.berquist@gmail.com>
2021-10-24 22:44:05 -0400

    [emacs-lisp][grains] add docstrings

--- emacs-lisp/grains/grains.el
+++ emacs-lisp/grains/grains.el
@@ -5,12 +5,14 @@
 ;;; Code:
 
 (defun square (square-num)
+  "Calculate the number of grains on the SQUARE-NUM square."
   (let ((tot 1))
     (dotimes (i (1- square-num))
       (setq tot (* tot 2)))
     tot))
 
 (defun total ()
+  "Calculate how many grains there are on the entire chessboard."
   ;; (apply '+ (mapcar 'square (number-sequence 1 64)))
   (1- (square (1+ 64))))
 

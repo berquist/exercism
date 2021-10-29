dcd6288c50fa8225df1ec9fc24e32789577d7edb
Eric Berquist <eric.berquist@gmail.com>
2021-10-28 21:22:26 -0400

    [emacs-lisp][pangram] implement seq-all

--- emacs-lisp/pangram/pangram.el
+++ emacs-lisp/pangram/pangram.el
@@ -7,7 +7,9 @@
 (require 'cl-seq)
 (require 'seq)
 
-;; (defun seq-all (seq pred))
+(defun seq-all (pred seq)
+  "Do all elements of SEQ satisfy PRED?"
+  (not (seq-contains-p (mapcar pred seq) nil)))
 
 (defconst lowercase-letters-as-char (string-to-list "abcdefghijklmnopqrstuvwxyz"))
 
@@ -16,8 +18,9 @@
 
 (defun is-pangram (sentence)
   (let ((cleaned-sentence (clean-sentence sentence)))
-    (not (seq-contains-p (mapcar (lambda (letter) (seq-contains-p cleaned-sentence letter)) lowercase-letters-as-char) nil))))
-
+    (seq-all
+     #'(lambda (letter) (seq-contains-p cleaned-sentence letter))
+     lowercase-letters-as-char)))
 
 (provide 'pangram)
 ;;; pangram.el ends here

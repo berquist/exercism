22e821226d771b82ad2c1f3708aa9d9d33500737
Eric Berquist <eric.berquist@gmail.com>
2021-10-28 22:15:03 -0400

    [python][guidos-gorgeous-lasagna]

--- python/guidos-gorgeous-lasagna/lasagna.py
+++ python/guidos-gorgeous-lasagna/lasagna.py
@@ -1,10 +1,8 @@
-# TODO: define the 'EXPECTED_BAKE_TIME' constant
-# TODO: consider defining the 'PREPARATION_TIME' constant
-#       equal to the time it takes to prepare a single layer
+EXPECTED_BAKE_TIME = 40
+PREPARATION_TIME = 2
 
 
-# TODO: define the 'bake_time_remaining()' function
-def bake_time_remaining():
+def bake_time_remaining(elapsed_bake_time: int) -> int:
     """Calculate the bake time remaining.
 
     :param elapsed_bake_time: int baking time already elapsed.
@@ -15,9 +13,12 @@ def bake_time_remaining():
     based on the `EXPECTED_BAKE_TIME`.
     """
 
-    pass
+    return EXPECTED_BAKE_TIME - elapsed_bake_time
 
-# TODO: define the 'preparation_time_in_minutes()' function
-#       and consider using 'PREPARATION_TIME' here
 
-# TODO: define the 'elapsed_time_in_minutes()' function
+def preparation_time_in_minutes(num_layers: int) -> int:
+    return PREPARATION_TIME * num_layers
+
+
+def elapsed_time_in_minutes(number_of_layers: int, elapsed_bake_time: int) -> int:
+    return elapsed_bake_time + preparation_time_in_minutes(number_of_layers)

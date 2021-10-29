0e8aee92554b796a715210ec8ee99e692d572b91
Eric Berquist <eric.berquist@gmail.com>
2021-10-28 22:53:24 -0400

    [python][markdown] more string interpolation

--- python/markdown/markdown.py
+++ python/markdown/markdown.py
@@ -6,7 +6,7 @@ RE_BOLD = re.compile('(.*)__(.*)__(.*)')
 
 
 def wrap_tag(m: re.Match, tag: str) -> str:
-    return m.group(1) + f'<{tag}>' + m.group(2) + f'</{tag}>' + m.group(3)
+    return f"{m.group(1)}<{tag}>{m.group(2)}</{tag}>{m.group(3)}"
 
 
 def wrap_bold_italics(s: str) -> str:
@@ -26,21 +26,21 @@ def parse(markdown: str) -> str:
     in_list_append = False
     for i in lines:
         if re.match('###### (.*)', i) is not None:
-            i = '<h6>' + i[7:] + '</h6>'
+            i = f'<h6>{i[7:]}</h6>'
         elif re.match('## (.*)', i) is not None:
-            i = '<h2>' + i[3:] + '</h2>'
+            i = f'<h2>{i[3:]}</h2>'
         elif re.match('# (.*)', i) is not None:
-            i = '<h1>' + i[2:] + '</h1>'
+            i = f'<h1>{i[2:]}</h1>'
         m = re.match(r'\* (.*)', i)
         if m:
             curr = m.group(1)
             if not in_list:
                 in_list = True
                 curr = wrap_bold_italics(curr)
-                i = '<ul><li>' + curr + '</li>'
+                i = f'<ul><li>{curr}</li>'
             else:
                 curr = wrap_bold_italics(curr)
-                i = '<li>' + curr + '</li>'
+                i = f'<li>{curr}</li>'
         else:
             if in_list:
                 in_list_append = True
@@ -48,10 +48,10 @@ def parse(markdown: str) -> str:
 
         m = re.match('<h|<ul|<p|<li', i)
         if not m:
-            i = '<p>' + i + '</p>'
+            i = f'<p>{i}</p>'
         i = wrap_bold_italics(i)
         if in_list_append:
-            i = '</ul>' + i
+            i = f'</ul>{i}'
             in_list_append = False
         res.append(i)
     if in_list:

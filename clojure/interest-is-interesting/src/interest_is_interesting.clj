2fef48ff0f83fb1aaa0a59be521b7783d1cd6a5e
Eric Berquist <eric.berquist@gmail.com>
2021-11-01 21:11:35 -0400

    [clojure][interest-is-interesting] incomplete

---
+++ clojure/interest-is-interesting/src/interest_is_interesting.clj
@@ -0,0 +1,23 @@
+(ns interest-is-interesting)
+
+(defn interest-rate
+  "Return the interest rate based on current balance."
+  [balance]
+  (cond
+    (< balance 0) -3.213
+    (< balance 1000) 0.5
+    (< balance 5000) 1.621
+    :else 2.475))
+
+(defn annual-balance-update
+  "Return an updated balance after applying the interest rate."
+  [balance]
+  (def rate (/ (bigdec (interest-rate balance)) 100))
+  (if (neg? rate)
+    (* balance (- 1 rate))
+    (* balance (+ 1 rate))))
+
+(defn amount-to-donate
+  "TODO: add docstring"
+  [balance tax-free-percentage]
+  )

79886c5bdfb1943dd502aa142a316bcf84c84816
Eric Berquist <eric.berquist@gmail.com>
2022-06-15 22:15:47 -0400

    [clojurescript][rna-transcription] now it makes more sense to use lambda sugar

--- clojurescript/rna-transcription/src/rna_transcription.cljs
+++ clojurescript/rna-transcription/src/rna_transcription.cljs
@@ -3,12 +3,10 @@
 
 (defn to-rna [dna]
   (s/join
-   (map
-    (fn [dna-base]
-      (case dna-base
-        "C" "G"
-        "G" "C"
-        "A" "U"
-        "T" "A"
-        (throw (js/Error "invalid base"))))
-    dna)))
+   (map #(case %
+           "C" "G"
+           "G" "C"
+           "A" "U"
+           "T" "A"
+           (throw (js/Error "invalid base")))
+        dna)))

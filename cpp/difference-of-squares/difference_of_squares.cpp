817e946f6213f0085bd17966a111c3c72a1a0110
Eric Berquist <eric.berquist@gmail.com>
2022-08-17 21:35:06 -0400

    [cpp][difference-of-squares] v2

--- cpp/difference-of-squares/difference_of_squares.cpp
+++ cpp/difference-of-squares/difference_of_squares.cpp
@@ -1,22 +1,32 @@
 #include "difference_of_squares.h"
+#include <algorithm>
 #include <cmath>
+#include <numeric>
+#include <vector>
 
 namespace difference_of_squares {
 
 unsigned square_of_sum(const unsigned n) {
-    unsigned ret = 0;
-    for (unsigned i = 0; i <= n; i++) {
-        ret += i;
-    }
-    return std::pow(ret, 2);
+    // unsigned ret = 0;
+    // for (unsigned i = 1; i <= n; i++) {
+    //     ret += i;
+    // }
+    // return std::pow(ret, 2);
+    std::vector<unsigned> range(n);
+    std::iota(range.begin(), range.end(), 1);
+    return std::pow(std::accumulate(range.begin(), range.end(), 0), 2);
 }
 
 unsigned sum_of_squares(const unsigned n) {
-    unsigned ret = 0;
-    for (unsigned i = 0; i <= n; i++) {
-        ret += std::pow(i, 2);
-    }
-    return ret;
+    // unsigned ret = 0;
+    // for (unsigned i = 1; i <= n; i++) {
+    //     ret += std::pow(i, 2);
+    // }
+    // return ret;
+    std::vector<unsigned> range(n);
+    std::iota(range.begin(), range.end(), 1);
+    std::for_each(range.begin(), range.end(), [](unsigned &i) { i = std::pow(i, 2); });
+    return std::accumulate(range.begin(), range.end(), 0);
 }
 
 unsigned difference(const unsigned n) {

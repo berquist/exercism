840f31e72a432b28927efa8eb5746bcb105145b4
Eric Berquist <eric.berquist@gmail.com>
2022-09-06 23:24:45 -0400

    [python][resistor-color]

---
+++ python/resistor-color/resistor_color.py
@@ -0,0 +1,20 @@
+_COLORS = [
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "grey",
+    "white",
+]
+
+
+def color_code(color):
+    return _COLORS.index(color)
+
+
+def colors():
+    return _COLORS

7aaa48846713276bbd5fc9e11b2101d292e7b182
Eric Berquist <eric.berquist@gmail.com>
2022-09-07 22:58:36 -0400

    [python][diffie-hellman]

---
+++ python/diffie-hellman/diffie_hellman.py
@@ -0,0 +1,13 @@
+import random
+
+
+def private_key(p: int) -> int:
+    return random.randint(2, p-1)
+
+
+def public_key(p: int, g: int, private: int) -> int:
+    return (g ** private) % p
+
+
+def secret(p: int, public: int, private: int) -> int:
+    return (public ** private) % p

75158a7a15cfe7afab687ea9b3e9d152329ad29b
Eric Berquist <eric.berquist@gmail.com>
2022-09-08 22:57:38 -0400

    [python][armstrong-numbers] no strings

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,3 +1,16 @@
+from typing import List
+
+
+def _get_digits(number: int) -> List[int]:
+    digits = []
+    while number >= 10:
+        digits.append(number % 10)
+        number //= 10
+    digits.append(number)
+    return digits
+
+
 def is_armstrong_number(number: int) -> bool:
-    digits = [int(dig) for dig in str(number)]
+    digits = _get_digits(number)
+    print(digits)
     return sum(dig ** len(digits) for dig in digits) == number

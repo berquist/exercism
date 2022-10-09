6445510045ef65d14167712d31f19be64cf07007
Eric Berquist <eric.berquist@gmail.com>
2022-10-09 16:27:21 -0400

    [emacs-lisp][perfect-numbers] fastest get-factors

--- emacs-lisp/perfect-numbers/perfect-numbers.el
+++ emacs-lisp/perfect-numbers/perfect-numbers.el
@@ -4,15 +4,17 @@
 
 ;;; Code:
 
-(require 'seq)
+(require 'cl-seq)
 
 (defun get-factors (number)
-  "Get all the factors of a NUMBER, not including the number itself."
-  (let ((factors '()))
-    (dolist (candidate (number-sequence 1 (/ number 2)))
-      (if (= (mod number candidate) 0)
-          (setq factors (cons candidate factors))))
-    factors))
+  "Get all the factors of a NUMBER, not including the number itself.
+
+Adapted from https://exercism.org/tracks/emacs-lisp/exercises/perfect-numbers/solutions/quigonjeff
+"
+  (let* ((candidates (number-sequence 1 (1+ (floor (sqrt number)))))
+         (low-factors (cl-remove-if-not #'(lambda (i) (= (mod number i) 0)) candidates))
+         (high-factors (mapcar #'(lambda (i) (/ number i)) low-factors)))
+    (cl-remove-duplicates (append low-factors high-factors) :test #'eql)))
 
 (defun sum (nums)
   (apply '+ nums))
@@ -21,7 +23,7 @@
   "Do something with NUMBER here."
   (if (> number 0)
       (let* ((factors (get-factors number))
-             (aliquot-sum (sum factors)))
+             (aliquot-sum (- (sum factors) number)))
         (cond
          ((> aliquot-sum number) 'abundant)
          ((< aliquot-sum number) 'deficient)

ba4ab359037d22fb6e6a137f468c50033ad79cf9
Eric Berquist <eric.berquist@gmail.com>
2022-10-15 16:40:44 -0400

    [clojurescript][bob]

---
+++ clojurescript/bob/src/bob.cljs
@@ -0,0 +1,25 @@
+(ns bob
+  (:require [clojure.string :as s]))
+
+(def question-mark "?")
+
+(defn is-silence [prompt]
+  (empty? (s/trim prompt)))
+
+(defn is-question [prompt]
+  (= (last (s/trim prompt)) question-mark))
+
+(defn is-yelling [prompt]
+  (and (= prompt (s/upper-case prompt))
+       (re-find #"[a-zA-Z]" prompt)))
+
+(defn is-yelling-question [prompt]
+  (and (is-yelling prompt) (is-question prompt)))
+
+(defn response-for [prompt]
+  (cond
+    (is-silence prompt) "Fine. Be that way!"
+    (is-yelling-question prompt) "Calm down, I know what I'm doing!"
+    (is-question prompt) "Sure."
+    (is-yelling prompt) "Whoa, chill out!"
+    :else "Whatever."))

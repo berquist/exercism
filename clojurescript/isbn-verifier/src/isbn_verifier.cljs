fb299def415ca7cb701539979470b89b757b8f8f
Eric Berquist <eric.berquist@gmail.com>
2022-10-15 16:05:12 -0400

    [clojurescript][isbn-verifier] update to match clojure

--- clojurescript/isbn-verifier/src/isbn_verifier.cljs
+++ clojurescript/isbn-verifier/src/isbn_verifier.cljs
@@ -1,15 +1,23 @@
 (ns isbn-verifier
   (:require [clojure.string :as str]))
 
-(defn- parse-number [s]
+(defn- ctos
+  "Convert a character to a string."
+  [cs]
+  cs)
+
+(defn- parse-int
+  "Parse an integer or character digit from a string."
+  [s]
   (let [parsed (js/parseInt s)
         isnan (js/isNaN parsed)]
     (if-not isnan
       parsed
       false)))
 
-(defn- parse-check-val [s]
-  (let [parsed (parse-number s)]
+(defn- parse-check-val [cs]
+  (let [s (ctos cs)
+        parsed (parse-int s)]
     (cond
       (= s "X") 10
       (and (number? parsed)
@@ -17,12 +25,12 @@
       :else parsed)))
 
 (defn isbn? [isbn]
-  (let [stripped (str/join (str/split isbn "-"))]
+  (let [stripped (str/join (str/split isbn #"-"))]
     (if (= (count stripped) 10)
       (let [front-nine (subs stripped 0 9)
             check-val (parse-check-val (nth stripped 9))]
-        (if (and check-val (number? (parse-number front-nine)))
-          (let [values (conj (into [] (map js/parseInt front-nine)) check-val)]
+        (if (and check-val (number? (parse-int front-nine)))
+          (let [values (conj (into [] (map parse-int front-nine)) check-val)]
             (zero? (mod (reduce + (map * values (range 10 0 -1))) 11)))
           false))
       false)))

8a341f99b84c9a11e422134e2120673666448ee6
Eric Berquist <eric.berquist@gmail.com>
2022-10-27 16:50:50 -0400

    [clojurescript][run-length-encoding] first pass

---
+++ clojurescript/run-length-encoding/src/run_length_encoding.cljs
@@ -0,0 +1,36 @@
+(ns run-length-encoding
+  (:require [clojure.string :as string]))
+
+;; adapted from https://rosettacode.org/wiki/Run-length_encoding#Clojure
+(defn run-length-encode
+  [plain-text]
+  (->> plain-text
+       (partition-by identity)
+       (mapcat (juxt count first))
+       (filter #(not= % 1))
+       (apply str)))
+
+(defn is-int [x]
+  (not (js/isNaN (js/parseInt x))))
+
+(defn- form-count [digits]
+  (if (empty? digits) 1 (js/parseInt (string/join (map str digits)))))
+
+(defn run-length-decode
+  [cipher-text]
+  (loop [acc []
+         digits []
+         ch (first cipher-text)
+         remaining (rest cipher-text)]
+    (cond
+    (nil? ch) (string/join acc)
+    (is-int ch) (recur
+                 acc
+                 (conj digits ch)
+                 (first remaining)
+                 (rest remaining))
+    :else (recur
+           (conj acc (string/join (repeat (form-count digits) ch)))
+           []
+           (first remaining)
+           (rest remaining)))))

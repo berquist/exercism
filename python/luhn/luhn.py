555ed4e6b0a7a861e7f39690e56461cb3426de18
Eric Berquist <eric.berquist@gmail.com>
2022-11-08 23:14:26 -0500

    [python][luhn]

---
+++ python/luhn/luhn.py
@@ -0,0 +1,42 @@
+import re
+from typing import List
+
+
+def _make_below_ten(num: int) -> int:
+    if num > 9:
+        return num - 9
+    return num
+
+
+def _scale_every_enth(nums: List[int], scale: int, enth: int) -> List[int]:
+    result = list()
+    counter = 1
+    for idx in range(len(nums), 0, -1):
+        if counter == enth:
+            result.append(nums[idx - 1] * scale)
+            counter = 1
+        else:
+            result.append(nums[idx - 1])
+            counter += 1
+    return list(reversed(result))
+
+
+class Luhn:
+    def __init__(self, card_num: str) -> None:
+        no_spaces = re.sub(r"\s", "", card_num)
+        self._has_junk = no_spaces != re.sub(r"[a-zA-Z\W]", "", no_spaces)
+        if not self._has_junk:
+            self._card_num = [int(dig) for dig in no_spaces]
+        else:
+            self._card_num = list()
+
+    def valid(self) -> bool:
+        """Is this a valid Luhn number?"""
+        return (
+            (not self._has_junk)
+            and (len(self._card_num) > 1)
+            and (
+                not sum(map(_make_below_ten, _scale_every_enth(self._card_num, 2, 2)))
+                % 10
+            )
+        )

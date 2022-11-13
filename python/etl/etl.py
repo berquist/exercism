fd268da25b0b7b5db833bfc45fcb2391bdeb1a64
Eric Berquist <eric.berquist@gmail.com>
2022-11-13 14:17:13 -0500

    [python][etl] don't need to check for duplicate keys

--- python/etl/etl.py
+++ python/etl/etl.py
@@ -4,10 +4,6 @@ from typing import Dict, Mapping, Iterable
 def transform(legacy_data: Mapping[int, Iterable[str]]) -> Dict[str, int]:
     ret = dict()
     for count, letters in legacy_data.items():
-        letters = (letter.lower() for letter in letters)
         for letter in letters:
-            if letter in ret:
-                ret[letter] += count
-            else:
-                ret[letter] = count
+            ret[letter.lower()] = count
     return ret

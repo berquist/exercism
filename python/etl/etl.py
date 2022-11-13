188bc7dd167e16a5653b06364456d438a6cbaf0b
Eric Berquist <eric.berquist@gmail.com>
2022-11-13 14:18:46 -0500

    [python][etl] fancy comprehension

--- python/etl/etl.py
+++ python/etl/etl.py
@@ -2,8 +2,8 @@ from typing import Dict, Mapping, Iterable
 
 
 def transform(legacy_data: Mapping[int, Iterable[str]]) -> Dict[str, int]:
-    ret = dict()
-    for count, letters in legacy_data.items():
-        for letter in letters:
-            ret[letter.lower()] = count
-    return ret
+    return {
+        letter.lower(): count
+        for count, letters in legacy_data.items()
+        for letter in letters
+    }

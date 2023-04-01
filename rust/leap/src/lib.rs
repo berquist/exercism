09deb7b76cd7d4b60671b91d8f10bdc0cccaa9e9
Eric Berquist <eric.berquist@gmail.com>
2023-04-01 11:07:27 -0400

    [rust][leap] reduce number of computations

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,6 +1,9 @@
 pub fn is_leap_year(year: u64) -> bool {
-    let by_four = year % 4 == 0;
-    let by_hundred = year % 100 == 0;
-    let by_four_hundred = year % 400 == 0;
-    (by_four && !by_hundred) || (by_four && by_hundred && by_four_hundred)
+    if year % 400 == 0 {
+        true
+    } else if year % 100 == 0 {
+        false
+    } else {
+        year % 4 == 0
+    }
 }

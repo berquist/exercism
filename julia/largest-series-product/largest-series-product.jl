531af425fa2923ab76170389735aed472a3fca4d
Eric Berquist <eric.berquist@gmail.com>
2023-04-01 14:18:23 -0400

    [julia][largest-series-product] simplify defensive part

--- julia/largest-series-product/largest-series-product.jl
+++ julia/largest-series-product/largest-series-product.jl
@@ -2,7 +2,7 @@ strtoints(str::AbstractString) = map(c -> parse(Int, c), collect(str))
 windows(v::AbstractVector, size::Int) = [v[i:i+size-1] for i in 1:length(v)-size+1]
 
 function largest_product(str::AbstractString, span::Int)
-    if (length(str) < span) || (span < 0)
+    if !(0 <= span <= length(str))
         throw(ArgumentError("invalid span"))
     elseif span == 0
         1

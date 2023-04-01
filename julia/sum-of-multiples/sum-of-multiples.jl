d22ab1499421988c07e7e99df95c08bb605f1e00
Eric Berquist <eric.berquist@gmail.com>
2023-04-01 14:50:47 -0400

    [julia][sum-of-multiples] use filter

--- julia/sum-of-multiples/sum-of-multiples.jl
+++ julia/sum-of-multiples/sum-of-multiples.jl
@@ -1,3 +1,4 @@
 function sum_of_multiples(limit, factors)
-    [num for num in 0:limit-1 if any(multiple -> iszero(num % multiple), setdiff(factors, [0]))] |> sum
+    factors = setdiff(factors, [0])
+    filter(num -> any(multiple -> iszero(num % multiple), factors), 0:limit-1) |> sum
 end

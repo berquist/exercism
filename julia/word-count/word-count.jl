528bb2cbaab561afa8897aeaec85349eb2dfa5ee
Eric Berquist <eric.berquist@gmail.com>
2023-04-01 21:43:49 -0400

    [julia][word-count] figure out single quote regex

--- julia/word-count/word-count.jl
+++ julia/word-count/word-count.jl
@@ -1,15 +1,6 @@
-function removesinglequote(s::AbstractString)
-    s = replace(s, r"(\w)'(\W)" => s"\1 \2")
-    s = replace(s, r"(\W)'(\w)" => s"\1 \2")
-    s = replace(s, r"(\W)'(\W)" => s"\1 \2")
-    s = replace(s, r"^'" => "")
-    s = replace(s, r"'$" => "")
-    s
-end
-
 function wordcount(sentence::AbstractString)
     counts = Dict()
-    for word in split(removesinglequote(replace(lowercase(sentence), r"[^'\w]+" => " ")))
+    for word in split(replace(lowercase(sentence), r"[^'\w]+|\B'|'\B" => " "))
         if haskey(counts, word)
             counts[word] += 1
         else

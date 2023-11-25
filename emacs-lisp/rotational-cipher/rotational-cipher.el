46b3b728dd93d3b67b082c6fa33ce5e4bb6ccb2d
Eric Berquist <eric.berquist@gmail.com>
2023-11-24 22:52:01 -0500

    [emacs-lisp][rotational-cipher] functional

--- emacs-lisp/rotational-cipher/rotational-cipher.el
+++ emacs-lisp/rotational-cipher/rotational-cipher.el
@@ -10,23 +10,20 @@
        (setq sc (- sc 26)))
      sc))
 
-;; A 65
-;; Z 90
-;; a 97
-;; z 122
-
 (defun rotate-char (c shift-key)
   (cond
+   ;; A-Z
    ((and (>= c 65) (<= c 90)) (rotate-char-branch 90))
+   ;; a-z
    ((and (>= c 97) (<= c 122)) (rotate-char-branch 122))
    (t c)))
 
 (defun rotate (text shift-key)
-  (let ((rotated '()))
-    (dolist (s (split-string text "" t))
-      (let ((c (string-to-char s)))
-        (push (char-to-string (rotate-char c shift-key)) rotated)))
-    (string-join (reverse rotated))))
+  (string-join
+   (mapcar
+    (lambda (s)
+      (char-to-string (rotate-char (string-to-char s) shift-key)))
+    (split-string text "" t))))
 
 
 (provide 'rotational-cipher)

75a8ea343e59d0c5b43d79bec7d3e050d5f0e46c
Eric Berquist <eric.berquist@gmail.com>
2024-11-19 11:16:03 -0500

    [csharp][leap] example of using standard library

--- csharp/leap/Leap.cs
+++ csharp/leap/Leap.cs
@@ -3,5 +3,6 @@ using System;
 public static class Leap
 {
     public static bool IsLeapYear(int year) =>
-        (year % 4 == 0) && ((year % 400 == 0) || !(year % 100 == 0));
+        DateTime.IsLeapYear(year);
+        // (year % 4 == 0) && ((year % 400 == 0) || !(year % 100 == 0));
 }
